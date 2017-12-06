a67d0a6397cd305fdffc00283c46be7b7bdb43ff
Rajesh Sivakumar <rajeshs@LT-MAC46.local>
2017-12-05 16:01:22 -0800

    Replaced reference to View.PropTypes with ViewPropTypes

--- Collapsible.js
+++ Collapsible.js
@@ -8,6 +8,7 @@ import {
   Animated,
   Easing,
   View,
+  ViewPropTypes
 } from 'react-native';
 
 const ANIMATED_EASING_PREFIXES = ['easeInOut', 'easeOut', 'easeIn'];
@@ -25,7 +26,7 @@ class Collapsible extends Component {
       PropTypes.string,
       PropTypes.func,
     ]),
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
   };
 
   static defaultProps = {
